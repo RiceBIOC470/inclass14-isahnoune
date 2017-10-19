d641f2d097cd434c884c752ab6059391ebf316ad
isahnoune <31284645+isahnoune@users.noreply.github.com>
2017-10-18 23:51:08 -0500

    Add files via upload

--- Inclass14.m
+++ Inclass14.m
@@ -3,6 +3,44 @@
 %Work with the image stemcells_dapi.tif in this folder
 
 % (1) Make a binary mask by thresholding as best you can
+
+
+file1 = 'stemcells_dapi.tif';
+reader1 = bfGetReader(file1);
+iplane = reader1.getIndex(1-1, 1-1, 1-1)+1;
+img11 = bfGetPlane(reader1, iplane);
+
+
+imshow(img11, [])
+binarymask = img11 > 350;
+imshow(binarymask,[])
+
 % (2) Try to separate touching objects using watershed. Use two different
-% ways to define the basins. (A) With erosion of the mask (B) with a
-% distance transform. Which works better in this case?
+% ways to define the basins. 
+
+%(A) With erosion of the mask 
+
+CC = bwconncomp(binarymask);
+stats = regionprops(CC, 'Area');
+area = (stats.Area);
+s = round(1.2*sqrt(mean(area))/pi);
+eroded = imerode(binarymask, strel('disk',s));
+outside = ~imdilate(binarymask, strel('disk',1));
+basin = imcomplement(bwdist(outside));
+basin = imimposemin(basin,eroded|outside);
+L = watershed(basin);
+imshow(L, [])
+
+%(B) with a distance transform. 
+
+D = bwdist(~binarymask);
+D = -D;
+D(~binarymask) = -Inf;
+L2 = watershed(D);
+imshow(L2,[])
+
+%Which works better in this case?
+
+%In this case, using the watershed method with erosion of the mask shows a more uniform and
+%filled image. The distance transform yields breaks throughout the cells,
+%with dark lines separating whole cells.
